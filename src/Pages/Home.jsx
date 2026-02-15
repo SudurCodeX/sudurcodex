b7181cae2d0a7cb6e643989a9406a836086edf34
maheshkhatri2058 <maheshkhatri2058@gmail.com>
2026-02-15 23:38:40 +0530

    sdvs

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -2,22 +2,11 @@ import React from 'react'
 
 const Home = () => {
   return (
-<<<<<<< main
-    <div>
-
-<h2 className='text-4xl font-bold'>
-    This is Home Page
-     </h2>   
-     <p>This is a codex webste</p>  
-     <h1>This is a heading</h1> 
-    </div>
-=======
     <>
     <section className='h-screen'>
        hello
     </section>
     </>
->>>>>>> local
   )
 }
 
