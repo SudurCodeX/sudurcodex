d7facf35c7230201fd76e7bc54facb77eeffce7c
maheshkhatri2058 <maheshkhatri2058@gmail.com>
2026-02-15 20:03:31 +0530

    First push

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../src/Pages/Home'
+
+function App() {
+  return (
+    <>
+     <Routes>
+      <Route path='/' element={<Home/>}></Route>
+
+     </Routes>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+
+<h2 className='text-4xl font-bold'>
+    This is Home Page
+     </h2>      
+    </div>
+  )
+}
+
+export default Home
