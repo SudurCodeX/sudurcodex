ab1ebc796f2207e48ec7771e86c9e5e3b2687756
Durpat <durpatbalayar2@gmail.com>
2026-02-17 00:44:27 +0530

    service edited

--- src/Other/ServiceCTA.jsx
+++ src/Other/ServiceCTA.jsx
@@ -3,12 +3,12 @@ import { useNavigate } from "react-router-dom";
 export default function ServiceCTA() {
   const navigate = useNavigate();
   return (
-    <section className="py-20 bg-[#606163a1] text-white text-center">
-      <h2 className="text-4xl font-bold mb-4">
+    <section className="py-20 bg-zinc-100 text-white text-center">
+      <h2 className="text-4xl font-bold mb-4 text-orange-500">
         Have an Idea? Let's Build It Together
       </h2>
 
-      <p className="mb-8 text-gray-400 max-w-2xl mx-auto">
+      <p className="mb-8 text-gray-900 max-w-2xl mx-auto">
         Whether you want to launch a startup, upgrade your business, or start
         your tech career we help you move faster.
       </p>
